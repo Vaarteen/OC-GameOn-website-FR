e1b9ba77a1eee4cc94930384bd108aa8ce4d8502
Herbert Caffarel <herbert.caffarel@cicef.pro>
2022-11-10 11:53:19 +0100

    Added HTML error messages in dataset Check and validate all inputs

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -20,19 +20,27 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 // Close modal event
 modalClose.addEventListener("click", closeModal);
 
-// launch modal form
+/**
+ * launch modal form
+ */
 function launchModal() {
   clearForm();
   modalbg.style.display = "block";
 }
 
-// close modal form
+/**
+ * close modal form
+ */
 function closeModal() {
   modalbg.style.display = "none";
 }
 
-// validate form content
+/**
+ * validate form content. Called in HTML form tag
+ * @returns true if form is correct, or false
+ */
 function validate() {
+  clearAllErrorMessages();
   isOk = true;
   // validate lastname & firstname
   const last = document.getElementById("last");
@@ -59,15 +67,16 @@ function validate() {
   }
   // validate quantity
   const qty = document.getElementById("quantity");
-  if (! checkQuantity) {
+  if (! checkQuantity(qty.value)) {
     isOk = false;
     showErrorMessage(qty,);
   }
   // validate radio button
-  const radio = document.querySelector("input[type='radio']:selected");
+  const radio = document.querySelector("input[type='radio']:checked");
   if (radio === null) {
     isOk = false;
-    showErrorMessage(radio);
+    // Warning! radio is null, must find a radio button to give to function
+    showErrorMessage(document.querySelector("input[type='radio']"));
   }
   // validate acceptance
   const accept = document.getElementById("checkbox1");
@@ -78,54 +87,84 @@ function validate() {
   return isOk;
 }
 
-// check string length against min length
-function checkLength(string, minLength) {
-  if (isNull(string) || string === undefined || string.length < minLength) {
+/**
+ * check string length against min length
+ * @param {String} str string to check against minLength
+ * @param {int} minLength minimum acceptable length for str
+ * @returns true if str matches minLength, or false
+ */
+function checkLength(str, minLength) {
+  if (str === null || str === undefined ||  str.length < minLength) {
     return false;
   }
   return true;
 }
 
-// check if email is valid email address
+/**
+ * check if email is valid email address
+ * @param {String} email string to check
+ * @returns true if email is valid email address
+ */
 function checkEmail(email) {
   // Simple regexp, but enough
   const pattern = /^[\w._%+-]+@[\w.-]+\.[A-Za-z]{2,}$/
   return pattern.test(email);
 }
 
-// check if birthdate is present
+/**
+ * check if birthdate is present
+ * @param {String} birth birthdate value
+ * @returns true if birthdate exists, or false
+ */
 function checkBirthdate(birth) {
-  return (! isNull(birth) && birth !== "");
+  return (birth !== null && birth !== undefined && birth !== "");
 }
 
-// chek if quantity is present
+/**
+ * chek if quantity is valid
+ * @param {*} qty quantity to check
+ * @returns true if qty is between 0 and 99
+ */
 function checkQuantity(qty) {
-  return Number.isInteger(parseInt(qty.value));
+  const value = parseInt(qty);
+  return Number.isInteger(value) ? (value >= 0 && value <= 99) : false;
 }
 
-// show  error message associated with element
+/**
+ * show  error message associated with element
+ * @param {HTMLElement} element HTMLElement in error
+ * @returns undefined if element is not given
+ */
 function showErrorMessage(element) {
+  if (! element) return;
   const div = element.closest(".formData");
   if (div) {
-    div.dataset["data-error"] = true;
+    div.dataset.errorVisible = true;
   }
 }
 
-// clear error message associated with element
+/**
+ * clear error message associated with element
+ * @param {HTMLElement} element element in error
+ */
 function clearErrorMessage(element) {
   const div = element.closest(".formData");
   if (div) {
-    delete div.dataset["data-error"];
+    delete div.dataset.errorVisible;
   }
 }
 
-// clear all error messages
+/**
+ * clear all error messages
+ */
 function clearAllErrorMessages() {
-  document.querySelectorAll("formData")
+  document.querySelectorAll(".formData")
     .forEach(elt => clearErrorMessage(elt));
 }
 
-// clear form
+/**
+ * reset form and clear all error messages
+ */
 function clearForm() {
   document.querySelector("form").reset();
   clearAllErrorMessages();
